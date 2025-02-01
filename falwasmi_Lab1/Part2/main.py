4b75222c389518629fa6646430220c988cb56e2c
Fatma-Alwasmi <hotbreakfast01@gmail.com>
2025-02-01 15:54:35 -0800

    Update main.py

--- falwasmi_Lab1/Part2/main.py
+++ falwasmi_Lab1/Part2/main.py
@@ -1,15 +1,28 @@
+import machine
 from machine import Pin
-from time import sleep
+import time
+from neopixel import NeoPixel
 
-led_board = Pin(0, Pin.OUT)
-
-# Toggle LED 5 times
-
-for i in range(10):
-    led_board.value(not led_board.value())
-    sleep(0.5)
-
-print("LED blinked 5 times")
-
+count = 0
+button = Pin(38,Pin.IN)
+NEOPIXEL_PIN = 0
+NEOPIXEL_POWER_PIN = 2
+power_pin = Pin(NEOPIXEL_POWER_PIN, Pin.OUT)
+power_pin.value(1)
+led = NeoPixel(machine.Pin(NEOPIXEL_PIN), 1)
+while True:
+    led[0] = (255,0,0)
+    led.write()
+    if button.value() == 0:
+        led[0] = (0,255,0)
+        led.write()
+        time.sleep(0.3)
+        print(count)
+        count += 1
+    if count == 5:
+        print("You have successfully implemented LAB1")
+        power_pin.value(0)
+        break
     
+
 
