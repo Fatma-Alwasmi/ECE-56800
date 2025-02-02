bf303675e402f561993f4912304563c7c46a06ca
Fatma-Alwasmi <hotbreakfast01@gmail.com>
2025-02-01 17:06:16 -0800

    Update main.py

--- falwasmi_Lab1/Part2/main.py
+++ falwasmi_Lab1/Part2/main.py
@@ -5,24 +5,18 @@ from neopixel import NeoPixel
 
 count = 0
 button = Pin(38,Pin.IN)
-NEOPIXEL_PIN = 0
-NEOPIXEL_POWER_PIN = 2
-power_pin = Pin(NEOPIXEL_POWER_PIN, Pin.OUT)
+power_pin = Pin(2, Pin.OUT)
 power_pin.value(1)
-led = NeoPixel(machine.Pin(NEOPIXEL_PIN), 1)
-while True:
-    led[0] = (255,0,0)
+led = NeoPixel(machine.Pin(0), 1)
+while count != 5:
+    led[0] = (255,0,0) #led ->red
     led.write()
-    if button.value() == 0:
-        led[0] = (0,255,0)
+    while button.value() == 0: # pressed
+        led[0] = (0,255,0) #led ->green
         led.write()
-        time.sleep(0.3)
         print(count)
-        count += 1
-    if count == 5:
-        print("You have successfully implemented LAB1")
-        power_pin.value(0)
-        break
-    
+        if button.value() == 1: #unpressed
+            count += 1
 
-
+print("You have successfully implemented LAB1")
+power_pin.value(0)
