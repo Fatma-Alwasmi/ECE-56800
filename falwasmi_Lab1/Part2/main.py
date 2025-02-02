12a2a08765e2936571515e2aa16c688d65ed564b
Fatma-Alwasmi <hotbreakfast01@gmail.com>
2025-02-01 17:21:42 -0800

    Update main.py

--- falwasmi_Lab1/Part2/main.py
+++ falwasmi_Lab1/Part2/main.py
@@ -8,13 +8,12 @@ button = Pin(38,Pin.IN)
 power_pin = Pin(2, Pin.OUT)
 power_pin.value(1)
 led = NeoPixel(machine.Pin(0), 1)
-while count != 5:
+while count < 5:
     led[0] = (255,0,0) #led ->red
     led.write()
     while button.value() == 0: # pressed
         led[0] = (0,255,0) #led ->green
         led.write()
-        print(count)
         if button.value() == 1: #unpressed
             count += 1
 
