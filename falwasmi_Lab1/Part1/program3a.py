57ed23685bbc7ca350950de3c2324c2d9edd0952
Fatma <hotbreakfast01@gmail.com>
2025-02-14 10:43:28 -0500

    Update program3a.py

--- falwasmi_Lab1/Part1/program3a.py
+++ falwasmi_Lab1/Part1/program3a.py
@@ -4,15 +4,14 @@ a = 0
 b = 1
 c = a
 
-while count != 0:
-    print(c, ",", end= " ")
+
+fib = []
+
+while count > 0:
+    fib.append(c)
     count -=1
     a, b = b, c
     c = a + b
 
-            
-    
-        
-    
-     
-    
+x = ", ".join(map(str, fib))
+print("The Fibonacci Sequence is: " + x )       
