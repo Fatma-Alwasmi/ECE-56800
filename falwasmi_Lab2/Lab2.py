71876db7cb53b6fb9aa590f55e265037c0c42742
Fatma <hotbreakfast01@gmail.com>
2025-02-24 20:01:55 -0500

    Update Lab2.py

--- falwasmi_Lab2/Lab2.py
+++ falwasmi_Lab2/Lab2.py
@@ -1,8 +1,7 @@
-import machine
-from machine import ADC
-from machine import PWM
+from machine import RTC, Timer, ADC, PWM, Pin
 import utime
 
+# get user input for date and time
 year = int(input("Year? "))
 month = int(input("Month? "))
 day = int(input("Day? "))
@@ -12,42 +11,39 @@ minute = int(input("Minute? "))
 second = int(input("Second? "))
 microsecond = int(input("Microsecond? "))
 
-rtc = machine.RTC()
+# initialize RTC
+rtc = RTC()
 rtc.datetime((year, month, day, weekday, hour, minute, second, microsecond))
 
-# Timer callback function to display date and time
+# timer callback function to display date and time
 def display_datetime(timer):
-    datetime = rtc.datetime() #retrieving current date and time
-    formatted_date = f"{datetime[0]:04d}-{datetime[1]:02d}-{datetime[2]:02d})"
+    datetime = rtc.datetime()  
+    formatted_date = f"{datetime[0]:04d}-{datetime[1]:02d}-{datetime[2]:02d}"
     formatted_time = f"{datetime[4]:02d}:{datetime[5]:02d}:{datetime[6]:02d}.{datetime[7]:06d}"
     print(f"Date: {formatted_date}\nTime: {formatted_time}\n")
 
-# Setup hardware timer
-timer = machine.Timer(0)
-timer.init(period=30000, mode=machine.Timer.PERIODIC, callback=display_datetime)
+# setup hardware timer for RTC display
+datetime_timer = Timer(0)
+datetime_timer.init(period=30000, mode=Timer.PERIODIC, callback=display_datetime)
+
+# setup ADC for potentiometer
+pot = Pin(36, Pin.IN)
+adc_pot = ADC(pot)
+adc_pot.atten(ADC.ATTN_11DB)  # attenuation for full range 0-3.3V
+
+# timer callback function to read input pot
+def adc_potentiometer(timer):
+    pot_value = adc_pot.read()  
 
 
-pot = Pin(36, Pin.IN) # input signal from potentiometer
+# setup hardware timer for pot readings
+pot_timer = Timer(1)
+pot_timer.init(period=100, mode=Timer.PERIODIC, callback=adc_potentiometer)
 
-def ADC_potentiometer(tim):
-    
-    adc_pot = ADC(pot) # convert pot signal from analog to digital
-    
-# Setup hardware timer for pot signal
-tim = machine.Timer(0)
-tim.init(preriod=100, mode=machine.Timer.PERIODIC, callback=ADC_potentiometer)
-
-
-led = Pin(13, Pin.OUT) # output LED
-
-pwm = PWM(led, freq=10, duty_u16 = 512)
-
-
-
-
+# setup PWM for LED
+led = Pin(13, Pin.OUT)
+pwm_led = PWM(led, freq=10, duty_u16=512)  
 
 while True:
-    pass
-
-
+    pass  
 
